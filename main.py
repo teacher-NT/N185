61a37ab0956ce838bedfe39e0e5e9472d8d2ef49
Mirjamol Mirislomov <alpholmon@gmail.com>
2025-12-15 15:50:54 +0500

    Hard

--- main.py
+++ main.py
@@ -1,45 +1,28 @@
 import os
 os.system("cls")
 
-# set1 = {1,2,3,4,10,11,15}
-# set1.add("salom")
+set1 = {1,2,3,4,5,6,7,8,9,10,11,12}
+set2 = {4,5,6,7,8,9}
+
+# print(set2.issubset(set1))
+# print(set1.issuperset(set2))
+
+# set3 = set1.intersection(set2)
+# print(set3)
+
+# set1.intersection_update(set2)
+# print(set1)
+
+# set3 = set1.difference(set2)
+# print(set3)
+
+# set1.difference_update(set2)
 # print(set1)
 
 
-# set1 = {1,2,3,4,10,11,15}
-# set1.remove(10)
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# set1.symmetric_difference_update(set2)
 # print(set1)
 
-# set1 = {5,6,1,2,3,4,10,11,15}
-# set1.pop()
-# set1.pop()
-# print(set1)
-
-# set1 = {1,2,3,4,10,11,15}
-# set1.discard(20)
-# print(set1)
-
-# set1 = {1,2,3,4,10,11,15}
-# set1.clear()
-# print(set1)
-
-
-# set1 = {1,2,3,4,10,11,15}
-# set2 = set1.copy()
-# set2.add(14)
-# print(set2)
-# print(set1)
-
-
-
-# set1 = {1,2,3,4,10,11,15}
-# lst = [6,7,8,9]
-# set1.update(lst)
-# print(set1)
-
-
-# set1 = {1,2,3,4,10,11,15}
-# lst = [6,7,8,9]
-# set2 = set1.union(lst)
-# print(set1)
-# print(set2)
