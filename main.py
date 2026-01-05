0745c6dda0e0aed8ad54c9a7e03958ab2cfd46bf
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-05 15:43:54 +0500

    binar fayl

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import os
 os.system("cls")
 
-# pip install opencv-python
-import cv2
-
-camera = cv2.VideoCapture(0)
-
-while True:
-    is_valid, image = camera.read()
-    if is_valid:
-        cv2.imshow("Ko'rsatish", image)
+with open("image.jpeg", "rb") as file:
+    baytes = file.read()
     
-    if cv2.waitKey(1) & 0xfff == 32:
-        break
-
-cv2.destroyAllWindows()
+with open('panda.jpeg', "wb") as file:
+    file.write(baytes)
