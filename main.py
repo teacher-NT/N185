2789f04ed2de8ccc55305223fd73fa60fb233b66
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-09 15:23:14 +0500

    OOP

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import os
 os.system("cls")
 
-with open("image.jpeg", "rb") as file:
-    baytes = file.read()
+class Product:
+    def __init__(self, n, p, s, w, t):
+        self.name = n
+        self.price = p
+        self.shelf_life = s
+        self.weight = w
+        self.type_fruit = t
+
+    def info(self):
+        print(f"Maxsulot: {self.name}, Narxi: {self.price}")
     
-with open('panda.jpeg', "wb") as file:
-    file.write(baytes)
+    def discount(self, foiz):
+        # self.price = self.price-(self.price/100*foiz)
+        self.price -= self.price/100*foiz
+    
+    def increase_price(self, foiz):
+        self.price = self.price+(self.price/100*foiz)
+
+
+p1 = Product("Yogurt", 12000, '1-oy', 500, 'Banan')
+p1.info()
+p1.discount(30)
+p1.info()
+p1.increase_price(50)
+p1.info()
