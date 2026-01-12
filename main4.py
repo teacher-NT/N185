528dd19fd51fbf29debd21295f4ff9521c5a5b02
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-12 15:35:16 +0500

    Gibrid

--- main4.py
+++ main4.py
@@ -1,3 +1,28 @@
 import os
 os.system("cls")
 
+class A:
+    def run(self):
+        print("A is runing...")
+
+class B(A):
+    def run(self):
+        print("B is runing...")
+
+class C(A):
+    def run(self):
+        print("C is runing...")
+
+class D(B,C,A):
+    pass
+
+print(D.mro())
+
+
+# class E(D, C):
+#     pass
+
+# e1 = E()
+# e1.run()
+# d1 = D()
+# d1.run()
