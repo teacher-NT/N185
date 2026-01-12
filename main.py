4e9a22c7904ba417cc8318c6bee9f64107b9555f
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-12 14:29:12 +0500

    Voris olish

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 import os
 os.system("cls")
 
-class Product:
-    def __init__(self, n, p, s, w, t):
-        self.name = n
-        self.price = p
-        self.shelf_life = s
-        self.weight = w
-        self.type_fruit = t
+class Transport:
+    def __init__(self, o_k, rang, brand, model, narx):
+        self.o_k = o_k
+        self.rang = rang
+        self.brand = brand
+        self.model = model
+        self.narx = narx
 
-    def info(self):
-        print(f"Maxsulot: {self.name}, Narxi: {self.price}")
-    
-    def discount(self, foiz):
-        # self.price = self.price-(self.price/100*foiz)
-        self.price -= self.price/100*foiz
-    
-    def increase_price(self, foiz):
-        self.price = self.price+(self.price/100*foiz)
+    def move(self):
+        print("Transport harakatlanmoqda...")
 
 
-p1 = Product("Yogurt", 12000, '1-oy', 500, 'Banan')
-p1.info()
-p1.discount(30)
-p1.info()
-p1.increase_price(50)
-p1.info()
+class Car(Transport):
+    pass
+
+class Plane(Transport):
+    pass
+
+car1 = Car(500, "Oq", "BMW", "M5", 128000)
+car1.move()
+
+plane1 = Plane(800, "Qora", "Boing", "&45", 20_000_000)
+plane1.move()
