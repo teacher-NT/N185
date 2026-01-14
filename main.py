e22e2f2f2f6d8797be3d87ef6634d3eda2b7821a
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-14 15:28:21 +0500

    Polimorfizm

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 import os
 os.system("cls")
 
-class Car:
-    def __init__(self, brand, model, rang, narx):
-        self.brand = brand
-        self.model = model
-        self.rang = rang
-        self.narx = narx
+class Person:
+    def __init__(self, ism,yosh,jins,manzil):
+        self.ism = ism
+        self.yosh = yosh
+        self.jins = jins
+        self.manzil = manzil
     
     def __str__(self):
-        return f"{self.brand} - {self.model}"
+        return f"Ism: {self.ism}. Yosh: {self.yosh} {self.jins} {self.manzil}"
     
-    def __add__(self, n):
-        self.narx += n
-        print("add metodi ishladi")
+    def __gt__(self, n):
+        return self.yosh > n
     
-    def __sub__(self, n):
-        self.narx -= n
+class Employee:
+    def __init__(self, ism, yosh, jins, manzil, tajriba, maosh):
+        self.ism = ism
+        self.yosh = yosh
+        self.jins = jins
+        self.manzil = manzil
+        self.tajriba = tajriba
+        self.maosh = maosh
+
+    def __str__(self):
+        return f"Ism: {self.ism}. {self.manzil} {self.maosh}"
 
     def __gt__(self, n):
-        return self.narx > n
-    
-    def __lt__(self, n):
-        return self.narx < n
-    
+        return self.maosh > n
 
 
-car1 = Car("GM", "Nexia", "Oq", 15000)
-car1 + 10
-car1 - 500
 
-print(car1 < 1000)
-print(car1 > 1000)
+p1 = Person("Abdulla", 15, "Erkak", "Toshkent sh.")
+e1 = Employee("Vali", 21, "Erkak", "Qo'qon", 3, 500)
+
+print(p1 > 20)
+print(e1 > 20)
