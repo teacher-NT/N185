16cd528264e304300fd5a800264e61094cdc39fd
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-01-19 15:27:09 +0500

    PushButton

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 os.system("cls")
 
-from PyQt5.QtWidgets import (QApplication, QWidget,QLabel)
+from PyQt5.QtWidgets import (QApplication, QWidget,
+                             QLabel, QPushButton)
 from PyQt5.QtGui import QFont
 
 font1 = QFont("Lobster", 20)
@@ -24,6 +25,16 @@ label2 = QLabel(oyna)
 label2.setText("0")
 label2.setStyleSheet("font-size:60px;")
 label2.move(160, 150)
+
+def func1():
+    print("Tugmacha bosildi...")
+
+btn1 = QPushButton(oyna)
+btn1.setText("Meni bos")
+btn1.setStyleSheet("background-color: #c9eb0c; color:black; font-size:25px; width:150px;height:50px;border-radius:25px;")
+btn1.move(130,250)
+btn1.clicked.connect(func1)
+
 
 oyna.show()
 app.exec_()
