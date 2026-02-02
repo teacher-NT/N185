aa793bdf974aea9739ec91c02adb5193de374c95
Mirjamol Mirislomov <alpholmon@gmail.com>
2026-02-02 15:25:38 +0500

    Mysql

--- main.py
+++ main.py
@@ -1,51 +1,37 @@
 import os
 os.system("cls")
 
-class Car:
-    def __init__(self, b,m,n,r):
-        self.brand = b
-        self.model = m
-        self.narx = n   
-        self.rang = r
+import mysql.connector
 
-    def info(self):
-        print(f"{self.brand} {self.model} {self.rang} {self.narx}")
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "1234",
+    database= "dars_baza"
+)
 
-    def show_price(self):
-        print(self.narx)
+query1 = """SELECT * FROM users 
+            WHERE joined_date > '2010-01-01';
+    """
 
-    def change_price(self, n):
-        if n < 20000:
-            print("Narx 20000dan kichik bo'lmasin")
-        else:
-            self.narx = n
+query2 = """INSERT INTO users (user_id, username, first_name, last_name, posts_count,followers, followings, joined_date)
+    VALUES
+    (33,'shamsiddin', 'Shamsiddin', 'Tursunov', 0, 2000, 15000, '2010-11-01')
 
-c1 = Car("BMW", "M5", 120000, "qora")
+"""
 
-# c1.narx = 25000
-c1.change_price(100000)
-c1.info()
-
-c2 = Car("Chevrolet", "Spark", 6000, "oq")
-c2.info()
-
-car3 = Car("Chevrolet", "Damas", 6000, "oq")
-car3.info()
-
-# print(c1.brand)
-# print(c1.narx)
+cursor = mydb.cursor()
+cursor.execute(query2)
+mydb.commit()
 
 
+# data = cursor.fetchall()
+# for qator in data:
+#     print(qator)
 
+# data = cursor.fetchone()
+# print(data)
 
-
-# a = 12
-# b = 2.3
-# d = "Salom"
-# e = True
-
-# print(type(a))
-# print(type(b))
-# print(type(d))
-# print(type(e))
-# print(type(c1))
+# data = cursor.fetchmany(5)
+# for qator in data:
+#     print(qator)
